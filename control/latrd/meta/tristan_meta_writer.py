c94c38992375ad6e5751b7fc867c0e5f8b3f5621
Alan Greer <alan.greer@diamond.ac.uk>
2020-11-19 14:04:47 +0000

    Updated Tristan writer to combine time slice datasets per module.

--- control/latrd/meta/tristan_meta_writer.py
+++ control/latrd/meta/tristan_meta_writer.py
@@ -5,19 +5,21 @@ meta data messages, writing them to disk
 """
 
 import re
+from collections import OrderedDict
 from json import loads
 import numpy as np
 import re
 import struct
 
-from odin_data.meta_writer.meta_writer import MetaWriter, MESSAGE_TYPE_ID, FRAME
+from odin_data.meta_writer.meta_writer import MetaWriter, MESSAGE_TYPE_ID, FRAME, require_open_hdf5_file
 from odin_data.meta_writer.hdf5dataset import Int32HDF5Dataset
 from odin_data.util import construct_version_dict
 import _version as versioneer
 
 # Dataset names
-DATASET_TIME_SLICE = "ts_rank"
+DATASET_TIME_SLICE = "ts_qty_module"
 DATASET_DAQ_VERSION = "data_version"
+DATASET_FP_PER_MODULE = "fp_per_module"
 
 # Data message parameters
 TIME_SLICE = "time_slice"
@@ -25,46 +27,67 @@ DAQ_VERSION = "daq_version"
 
 class TristanMetaWriter(MetaWriter):
     """ Implementation of MetaWriter that also handles Tristan meta messages """
+    TRISTAN_DATASETS = []
 
-    def __init__(self, name, directory, process_count):
-        # Add the version and time_slice datasets
-        MetaWriter.DETECTOR_DATASETS = [
-            Int32HDF5Dataset(DATASET_DAQ_VERSION)
-        ]
-        for index in range(process_count):
-            MetaWriter.DETECTOR_DATASETS.append(Int32HDF5Dataset("{}_{}".format(DATASET_TIME_SLICE, index)))
+    def __init__(self, name, directory, process_count, endpoints):
 
-        # Add the parameters received on meta messages
-        MetaWriter.DETECTOR_WRITE_FRAME_PARAMETERS = [
-            TIME_SLICE,
-            DAQ_VERSION
-        ]
+        # Create ordered dict to store FP mapping entries
+        self._fp_mapping = OrderedDict()
+        for endpoint in endpoints:
+            protocol, ip, port = endpoint.split(':')
+            ip = ip.strip('//')
+            if ip in self._fp_mapping:
+                self._fp_mapping[ip] += 1
+            else:
+                self._fp_mapping[ip] = 1
 
-        super(TristanMetaWriter, self).__init__(name, directory, process_count)
-        for dset in MetaWriter.DETECTOR_DATASETS:
-            self._logger.debug("Tristan dataset added: {} [{}]".format(dset.name, dset.dtype))
 
-    def process_message(self, header, data):
-        """ Process a message from a data socket
+        # Create the tristan specific datasets
+        TristanMetaWriter.TRISTAN_DATASETS.append(Int32HDF5Dataset(DATASET_DAQ_VERSION))
+        TristanMetaWriter.TRISTAN_DATASETS.append(Int32HDF5Dataset(DATASET_FP_PER_MODULE))
+        
+        for index in range(len(self._fp_mapping)):
+            TristanMetaWriter.TRISTAN_DATASETS.append(Int32HDF5Dataset("{}{:02d}".format(DATASET_TIME_SLICE, index)))
 
-        Check for Tristan specific messages and pass anything else to base class
-        """
-        self._logger.debug("Message ID: {}".format(header[MESSAGE_TYPE_ID]))
+        # Now that we have defined the datasets we can call the base class constructor
+        super(TristanMetaWriter, self).__init__(name, directory, process_count, endpoints)
+
+        # Log the current dataset configuration
+        self._logger.info("Set up Tristan writer for endpoints: {}".format(endpoints))
+        for dset in TristanMetaWriter.TRISTAN_DATASETS:
+            self._logger.info("Tristan dataset added: {} [{}]".format(dset.name, dset.dtype))
+
+    @property
+    def detector_message_handlers(self):
         message_handlers = {
             TIME_SLICE: self.handle_time_slice,
             DAQ_VERSION: self.handle_daq_version
         }
+        return message_handlers
 
-        handler = message_handlers.get(header[MESSAGE_TYPE_ID], None)
-        if handler is not None:
-            handler(header["header"], data)
-        else:
-            self._logger.debug(
-                "%s | Passing message %s to base class",
-                self._name,
-                header[MESSAGE_TYPE_ID],
-            )
-            super(TristanMetaWriter, self).process_message(header, data)
+    @staticmethod
+    def _define_detector_datasets():
+        print("METHOD CALLED: {}".format(TristanMetaWriter.TRISTAN_DATASETS))
+        return TristanMetaWriter.TRISTAN_DATASETS
+
+    @require_open_hdf5_file
+    def _create_datasets(self, dataset_size):
+        super(TristanMetaWriter, self)._create_datasets(dataset_size)
+        self._logger.info("Hooked into create datasets...")
+        # Save the FP mapping values
+        index = 0
+        for ip in self._fp_mapping:
+            self._add_value(DATASET_FP_PER_MODULE, self._fp_mapping[ip], offset=index)
+            index += 1
+
+    def rank_to_module(self, rank):
+        module = 0
+        for ip in self._fp_mapping:
+            if rank >= self._fp_mapping[ip]:
+                module += 1
+                rank -= self._fp_mapping[ip]
+        self._logger.debug("Rank {} calculated to module {}".format(rank, module))
+        return module
 
     def handle_time_slice(self, user_header, data):
         """Handle a time slice message.  Write the time slice information
@@ -83,10 +106,15 @@ class TristanMetaWriter(MetaWriter):
         array = struct.unpack(format_str, data)
         self._logger.debug("Rank: {}  Index {}  Array_Size {}".format(rank, index, array_size))
         self._logger.debug(array)
+        module = self.rank_to_module(rank)
 
-        dataset_name = "{}_{}".format(DATASET_TIME_SLICE, rank)
-        for value in array:
-            self._add_value(dataset_name, value)
+        dataset_name = "{}{:02d}".format(DATASET_TIME_SLICE, module)
+        # Now loop over provided data, and write any non-zero values
+        for array_index in range(len(array)):
+            if array[array_index] > 0:
+                offset = index + array_index
+                self._logger.debug("Adding value {} to offset {}".format(array[array_index], offset))
+                self._add_value(dataset_name, array[array_index], offset=offset)
 
     def handle_daq_version(self, user_header, data):
         """Handle a time slice message.  Write the time slice information
